d5552f557193c886cbd9c322bfd63e61e636cc9c
Mrlucagames <MrLuca@Lucas-MacBook-Pro.local>
2014-07-26 17:40:04 +0200

    initial version of tile generator

---
+++ tiles/Assets/tileGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tileGenerator : MonoBehaviour {
+
+	public GameObject[] tiles;
+
+
+	static int mapHeight = 32;
+	static int mapWidth = 24;
+	private int[,] map = new int[mapHeight,mapWidth] ; 
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (tiles.GetLength (0));
+		for (int i=0;i < map.GetLength(0); i++){
+			for(int j=0;j < map.GetLength(1);j++){
+				int elem = Random.Range(-3,4);
+				map[i,j] = elem;
+				Instantiate(tiles[elem+3],new Vector3(i,j,0),Quaternion.identity); 
+				//Instantiate(tiles[elem+3]); 
+			}
+		}
+		//string.Join(",",map.select(
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
